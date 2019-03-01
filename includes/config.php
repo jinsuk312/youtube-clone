14c7fdea4dc7af81384de00f96eaab23a4661983
jinsukmik <jinsukmik@gmail.com>
2019-03-01 11:06:50 -0600

    added config file to mysql db

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+ob_start(); // turns on output buffering, makes the output wait till everythings done loading
+
+date_default_timezone_set("America/Chicago");
+
+try{
+    $con = new PDO("mysql:dbname=VideoTube;host=localhost", "root", ""); 
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // error handling
+}
+catch(PDOException $e){
+    echo "Connection Failed: " . $e->getMessage();
+}
+
+?>
