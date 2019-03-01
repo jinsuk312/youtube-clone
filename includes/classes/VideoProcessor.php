6bbf3420b4b630bc6c29ca1e51744cb29e2287a0
jinsukmik <jinsukmik@gmail.com>
2019-03-01 13:45:56 -0600

    sucessful file moving on submit

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -1,6 +1,10 @@
 <?php
 class VideoProcessor{
     private $con;
+    // half a gb
+    private $sizeLimit = 500000000;
+    private $allowedTypes = array("mp4", "flv", "webm", "mkv", "vob", "ogv", "ogg", "avi", "wmv", "mov", "mpeg", "mpg");
+
     public function __construct($con){
         $this->con = $con;
     }
@@ -11,7 +15,42 @@ class VideoProcessor{
         $tempFilePath = $targetDir . uniqid() . basename($videoData["name"]);
         $tempFilePath = str_replace(" ", "_", $tempFilePath);
 
-        echo $tempFilePath;
+        $isValidData = $this->processData($videoData, $tempFilePath);
+        if(!isValidData){
+            return false;
+        }
+        if(move_uploaded_file($videoData["tmp_name"], $tempFilePath)){
+            echo "File moved sucessfully";
+        }
+    }
+
+    private function processData($videoData, $filePath){
+        $videoType = pathInfo($filePath, PATHINFO_EXTENSION);
+        if(!$this->isValidSize($videoData)){
+            echo "File too large. Can't be more than " . $this->sizeLimit . " bytes";
+            return false;
+        }
+        else if(!$this->isValidType($videoType)){
+            echo "Invalid file type";
+            return false;
+        }
+        else if($this->hasError($videoData)){
+            echo "Error code: " . $videoData["error"];
+            return false;
+        }
+        return true;
+    }
+
+    private function isValidSize($data){
+        return $data["size"] <= $this->sizeLimit;
+    }
+
+    private function isValidType($type){
+        $lowercased = strtolower($type);
+        return in_array($lowercased, $this->allowedTypes);
+    }
+    private function hasError($data){
+        return $data["error"] != 0;
     }
 }
 ?>
