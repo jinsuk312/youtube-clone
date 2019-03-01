d3419c8cd501ef357890dba3a57c9a69d533c638
jinsukmik <jinsukmik@gmail.com>
2019-03-01 00:38:25 -0600

    added jquery and bootstrap

--- index.php
+++ index.php
@@ -4,14 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./assets/css/style.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <title>VideoTube</title>
 </head>
 <body>
     <div id="pageContainer">
-        <div id="mastHeadContainer">hello</div>
+        <div id="mastHeadContainer"><button class="navShowHide">Click me!</button></div>
         <div id="sideNavContainer"></div>
-        <div id="mainSectionContainer"></div>
+        <div id="mainSectionContainer">
+            <div id="mainContentContainer">hello everyone</div>
+        </div>
     </div>
 </body>
 </html>
