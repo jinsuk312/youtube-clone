f4623a236127bac16ede108e79e24e814c129383
jinsukmik <jinsukmik@gmail.com>
2019-03-03 01:30:04 -0600

    finished user input validation

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -4,8 +4,13 @@
         public static $lastNameCharacters = "Your last name must be between 2 and 25 characters.";
         public static $usernameCharacters = "Your username must be between 5 and 25 characters.";
         public static $usernameTaken = "This username already exists.";
-        public static $emails = "Your emails do not match.";
+        public static $emailsDoNotMatch = "Your emails do not match.";
         public static $emailInvalid = "Please enter a valid email address.";
         public static $emailTaken = "This email is already in use.";
+        public static $passwordsDoNotMatch = "Your passwords do not match.";
+        public static $passwordNotAlphanumeric = "Your password can only contain numbers and letters.";
+        public static $passwordLength= "Your password must be between 5 and 30 characters.";
+        
+
     }
 ?>
